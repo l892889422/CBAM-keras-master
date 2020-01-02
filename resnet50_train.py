60e0a0e59c5f49baf11c328dbc7fbebb2310851d
Administrator <892889422@qq.com>
2020-01-02 13:52:14 +0800

    z

--- resnet50_train.py
+++ resnet50_train.py
@@ -20,8 +20,8 @@ from keras.preprocessing import image
 #VALID_DIR = os.path.join(DATA_DIR, 'validation')
 #VALID_DIR='G:/python/untitled1/demo/data/validation/test'
 
-TRAIN_DIR='D:/AI1403\ljy/CBAM-keras-master/数据集/train'
-VALID_DIR='D:/AI1403\ljy/CBAM-keras-master/数据集/val'
+TRAIN_DIR='D:/AI1403\ljy/数据集/train'
+VALID_DIR='D:/AI1403\ljy/数据集/val'
 SIZE = (224, 224)
 BATCH_SIZE = 2       #每次送入的数据
 
@@ -80,28 +80,28 @@ if __name__ == "__main__":
     batches = gen.flow_from_directory(TRAIN_DIR, target_size=SIZE, class_mode='categorical', shuffle=True, batch_size=BATCH_SIZE)
     val_batches = val_gen.flow_from_directory(VALID_DIR, target_size=SIZE, class_mode='categorical', shuffle=True, batch_size=BATCH_SIZE)
 
-    model = keras.applications.resnet50.ResNet50()          #预训练模型
+    model = keras.applications.resnet.ResNet50(include_top=False, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=5)        #预训练模型
 
-    classes = list(iter(batches.class_indices))             #用训练好的模型预测时，预测概率序列和Labels的对应关系
-    model.layers.pop()    #弹出模型的最后一层
-
-    #一个层意味着将其排除在训练之外，即其权重将永远不会更新。
-    for layer in model.layers:
-        layer.trainable=False
-
-    last = model.layers[-1].output  #输出
-    #全连接层 神经元数量和激活函数
-    print('神经元数量',len(classes))
-    # last = Dropout()(last)
-    x = Dense(len(classes), activation="softmax")(last)
-    print(model.layers[-1].name)
-    finetuned_model = Model(model.input, x)
+    # classes = list(iter(batches.class_indices))             #用训练好的模型预测时，预测概率序列和Labels的对应关系
+    # model.layers.pop()    #弹出模型的最后一层
+    #
+    # #一个层意味着将其排除在训练之外，即其权重将永远不会更新。
+    # for layer in model.layers:
+    #     layer.trainable=False
+    #
+    # last = model.layers[-1].output  #输出
+    # #全连接层 神经元数量和激活函数
+    # print('神经元数量',len(classes))
+    # # last = Dropout()(last)
+    # x = Dense(len(classes), activation="softmax")(last)
+    # print(model.layers[-1].name)
+    # finetuned_model = Model(model.input, x)
 
     # 设置损失函数，优化器，模型在训练和测试时的性能指标
-    finetuned_model.compile(optimizer=Adam(lr_schedule(0)), loss='categorical_crossentropy', metrics=['accuracy'])
-    for c in batches.class_indices:
-        classes[batches.class_indices[c]] = c
-    finetuned_model.classes = classes
+    model.compile(optimizer=Adam(lr_schedule(0)), loss='categorical_crossentropy', metrics=['accuracy'])
+    # for c in batches.class_indices:
+    #     classes[batches.class_indices[c]] = c
+    # finetuned_model.classes = classes
     #早停法防止过拟合，patience: 当early stop被激活(如发现loss相比上一个epoch训练没有下降)，则经过patience个epoch后停止训练
     #early_stopping = EarlyStopping(patience=10)
     checkpointer = ModelCheckpoint('resnet50_cbam.h5', verbose=1, save_best_only=True)  # 添加模型保存点
@@ -113,8 +113,8 @@ if __name__ == "__main__":
     TensorBoardcallback = keras.callbacks.TensorBoard(log_dir='./logs')
     callbacks = [checkpointer, lr_reducer, lr_scheduler,TensorBoardcallback]
     # 拟合模型
-    finetuned_model.fit_generator(batches, steps_per_epoch=num_train_steps, epochs=60,
+    model.fit_generator(batches, steps_per_epoch=num_train_steps, epochs=60,
                                   callbacks=callbacks, validation_data=val_batches,
                                   validation_steps=num_valid_steps)
     #拟合模型
-    finetuned_model.save('resnet50_final.h5')
+    model.save('resnet50_final.h5')
