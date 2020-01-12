c89bff2efec3f97ce6a6d84e49ddbf67e7728d43
jy <892889422@qq.com>
2020-01-12 10:54:54 +0800

    12

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ from keras.layers import Dense, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
 from utils import lr_schedule
-TRAIN_DIR='D:/AI1403/ljy/数据集/train'
-VALID_DIR='D:/AI1403/ljy/数据集/val'
+import seResnet50
+TRAIN_DIR='G:/python/train'
+VALID_DIR='G:/python/val'
 SIZE = (224, 224)
 BATCH_SIZE = 64       #每次送入的数据
 if __name__ == "__main__":
@@ -19,52 +20,13 @@ if __name__ == "__main__":
 
     gen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)            #图片生成器
     val_gen = keras.preprocessing.image.ImageDataGenerator()
-    # val_gen = keras.preprocessing.image.ImageDataGenerator( # set input mean to 0 over the dataset
-    #     featurewise_center=False,
-    #     # set each sample mean to 0
-    #     samplewise_center=False,
-    #     # divide inputs by std of dataset
-    #     featurewise_std_normalization=False,
-    #     # divide each input by its std
-    #     samplewise_std_normalization=False,
-    #     # apply ZCA whitening
-    #     zca_whitening=False,
-    #     # epsilon for ZCA whitening
-    #     zca_epsilon=1e-06,
-    #     # randomly rotate images in the range (deg 0 to 180)
-    #     rotation_range=90,
-    #     # randomly shift images horizontally
-    #     width_shift_range=0.1,
-    #     # randomly shift images vertically
-    #     height_shift_range=0.1,
-    #     # set range for random shear
-    #     shear_range=0.,
-    #     # set range for random zoom
-    #     zoom_range=0.,
-    #     # set range for random channel shifts
-    #     channel_shift_range=0.,
-    #     # set mode for filling points outside the input boundaries
-    #     fill_mode='nearest',
-    #     # value used for fill_mode = "constant"
-    #     cval=0.,
-    #     # randomly flip images
-    #     horizontal_flip=True,
-    #     # randomly flip images
-    #     vertical_flip=True,
-    #     # set rescaling factor (applied before any other transformation)
-    #     rescale=1./255,
-    #     # set function that will be applied on each input
-    #     preprocessing_function=None,
-    #     # image data format, either "channels_first" or "channels_last"
-    #     data_format=None,
-    #     # fraction of images reserved for validation (strictly between 0 and 1)
-    #     validation_split=0.0)
-    #以文件夹路径为参数,生成经过数据提升/归一化后的数据,在一个无限循环中无限产生batch数据
-    #路径、图像尺寸、返回标签数组的形式、是否打乱数据、batch数据的大小，默认为32
+
     batches = gen.flow_from_directory(TRAIN_DIR, target_size=SIZE, class_mode='categorical', shuffle=True, batch_size=BATCH_SIZE)
     val_batches = val_gen.flow_from_directory(VALID_DIR, target_size=SIZE, class_mode='categorical', shuffle=True, batch_size=BATCH_SIZE)
+    #model=keras.applications.resnet50.ResNet50(include_top=False, weights='imagenet', input_tensor=None, input_shape=None, pooling='avg', classes=5)
+    model=seResnet50(img_input=(224,224,3),classes_num=5)
 
-    model = keras.applications.resnet.ResNet50(include_top=False, weights='imagenet', input_tensor=None, input_shape=None, pooling='avg', classes=5)        #预训练模型
+    #model = keras.applications.resnet.ResNet50(include_top=False, weights='imagenet', input_tensor=None, input_shape=None, pooling='avg', classes=5)        #预训练模型
     classes = list(iter(batches.class_indices))             #用训练好的模型预测时，预测概率序列和Labels的对应关系
     # model.layers.pop()    #弹出模型的最后一层
     #
@@ -79,7 +41,7 @@ if __name__ == "__main__":
     x = Dense(len(classes), activation="softmax")(last)
     print(model.layers[-1].name)
     model = Model(model.input, x)
-
+    model.summary()
     # 设置损失函数，优化器，模型在训练和测试时的性能指标
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     # for c in batches.class_indices:
